56f2916dc93b851a19f63f99b2a04efaff47d10f
praveenviru <praveenbogam7@gmail.com>
2024-11-19 13:26:36 +0530

    Code updated....

---
+++ evenodd1.js
@@ -0,0 +1,13 @@
+const numbers=[1,2,3,4,5,6,7,8,9,11,10,12,13,14,15,17,18,19,20,21,24,12,45,23,1,5,46];
+let even="";
+let odd="";
+for(let i=0;i<numbers.length;i++){
+    if(numbers[i]%2==0){
+      even=even+numbers[i]+" ";
+    }
+    else{
+        odd=odd+numbers[i]+" ";
+    }
+}
+ console.log("Even = ",even);
+ console.log("odd= ",odd);
